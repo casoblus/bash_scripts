53cf80bb190c63c492e1806199c9b5071afaf7ea
Carlos Alberto Sobrino <casobrinolusquinos@gmail.com>
2017-04-25 22:23:15 +0200

    Added support to install aufs support

--- kernel_updater.sh
+++ kernel_updater.sh
@@ -1,19 +1,63 @@
 #!/bin/bash
-
 version=linux-$1
-
 cd /usr/src
+#rm $version.tar.xz*
+if [ -d $version ]; 
+then
+   cp $version/.config $version-config
+   rm -r $version
+   if ! [ -e $version.tar.xz ];
+   then
+      wget https://cdn.kernel.org/pub/linux/kernel/v4.x/$version.tar.xz
+   fi
+   tar -xJf $version.tar.xz 
+   cp $version-config $version/.config
+else
+   if ! [ -e $version.tar.xz ];
+   then
+      wget https://cdn.kernel.org/pub/linux/kernel/v4.x/$version.tar.xz
+   fi
+   tar -xJf $version.tar.xz 
+fi
 
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/$version.tar.xz
+#
+# INCLUDE AUFS SUPORT
+#
+if [ "$2" = "--with-aufs" ];
+then
+   echo "YOU MAY ACTIVATE AUFS-SUPPORT ON MENUCONFIG"
+   aufs_version=$(echo $1 | awk -F. '{print $1,$2}' | tr ' ' '.')
+   if [ -d aufs4-standalone ];
+   then
+      rm -r aufs4-standalone
+   fi
+   echo $aufs_version
+   git clone -b aufs$aufs_version --single-branch https://github.com/sfjro/aufs4-standalone.git aufs4-standalone
 
-tar -xJf $version.tar.xz 
+   cd /usr/src/$version
+   #patch -p1 < /usr/src/$version/0001-base-packaging.patch
+   #patch -p1 < /usr/src/$version/0002-debian-changelog.patch
+   #patch -p1 < /usr/src/$version/0003-configs-based-on-Ubuntu-4.0.2-1.1.patch
 
-if [ test -x linux/.config ]; then
+   patch -p1 < /usr/src/aufs4-standalone/aufs4-base.patch || exit
+   patch -p1 < /usr/src/aufs4-standalone/aufs4-standalone.patch || exit
+   patch -p1 < /usr/src/aufs4-standalone/aufs4-mmap.patch || exit
+   patch -p1 < /usr/src/aufs4-standalone/aufs4-kbuild.patch || exit
+
+   cp -R /usr/src/aufs4-standalone/Documentation /usr/src/$version || exit
+   cp -R /usr/src/aufs4-standalone/fs /usr/src/$version || exit
+   cp /usr/src/aufs4-standalone/include/uapi/linux/aufs_type.h /usr/src/$version/include/uapi/linux/. || exit
+fi
+
+#configureKernel {
+if [ -e linux/.config ]; 
+then
    cp linux/.config $version/.config 
    rm linux
    ln -s /usr/src/$version /usr/src/linux
    cd linux
    make oldconfig
+   make menuconfig
 else
    rm linux
    ln -s /usr/src/$version /usr/src/linux
@@ -22,15 +66,14 @@ else
 fi
 
 make-kpkg --initrd kernel_image kernel_headers modules -j9
-
 cd ..
 
-echo "¿Instalar ahora el nuevo kernel? [s|n]"
+echo "Install new kernel now? [s|n]"
 read confirm
 if [ "$confirm" == "s" ]; then
    dpkg -i linux-*-$1*.deb
 else
-   echo "Deberá instalar el paquete a mano."
+   echo "You must install package by yourself."
 fi
 
 echo "Para comenzar a usar el nuevo kernel deberá reiniciar ¿Reiniciar ahora? [s|n]"
