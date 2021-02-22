6d00c9c4ae154cf6485fa619d7b887e4ff773cb6
Carlos Alberto Sobrino <casobrinolusquinos@gmail.com>
2021-02-23 00:21:07 +0100

    Version 1 y descargo de responsabilidad

--- autoresize_images.sh
+++ autoresize_images.sh
@@ -1,54 +1,6 @@
 #!/bin/bash
 
-directory=$1
-
 set -euxo pipefail
-
-if [ $2 ]; 
-then
-	max_dimension=$2
-else
-	max_dimension=1920
-fi
-
-cd $directory
-
-if [ ! -d ../optimizedImages ]; 
-then
-	mkdir optimizedImages
-fi
-
-for i in $(ls);
-do 
-	w=$(identify -format "%w");
-	h=$(identify -format "%h");
-	mime1=$(mimetype $i | awk -F"/" '{print $1}')
-	mime2=$(mimetype $i | awk -F"/" '{print $2}')
-	if [ "$mime1" == "image" ]; 
-	then
-		if [ "$w" -gt "$max_dimension" ] || [ "$h" -gt "$max_dimension"]; 
-		then
-
-			convert $i -resize ${max_dimension}x${max_dimension} $i
-
-		fi
-		
-		case "$mime2" in
-			"png")
-				$(file $i | grep RGBA)
-				if [ $? -ne 0 ];
-				then
-				# No tiene transparencia
-					convertirjpeg $i
-				fi
-				optipng -nc -nb -o7 -full -out $i $i
-				;;
-			"jpeg")
-				jpegoptim --max=80 --all-progressive --strip-all --preserve --totals $i
-				;;
-		esac
-	fi
-done
 
 
 function convertirjpeg () {
@@ -60,3 +12,71 @@ function convertirjpeg () {
 	# Convierte a jpeg en la carpeta alternativa.
 	convert $1 jpegAlternatives/$(identify -format "%t" $1).jpeg
 }
+
+# Comprueba instalación de ImageMagick
+which magick > /dev/null 2>&1
+if [ $? -ne 0 ];
+then
+	echo It requires ImageMagick package installed.
+	exit 1
+fi
+
+if [[ $# > 2 ]]; then
+	echo Demasiados argumentos
+	exit 2
+fi
+
+case $# in
+	1)
+		directory=$1
+		max_dimension=1920
+		;;
+	2)
+		directory=$1
+		max_dimension=$2
+		;;
+	*)
+		directory=pwd
+		max_dimension=1920
+		;;
+esac
+
+cd $directory
+
+if [ ! -d "optimizedImages" ]; 
+then
+	mkdir optimizedImages
+fi
+
+for i in $(ls);
+do 
+	mime1=$(mimetype $i | awk '{print $2}' | awk -F"/" '{print $1}')
+	mime2=$(mimetype $i | awk -F"/" '{print $2}')
+	if [ "$mime1" == "image" ]; 
+	then
+		w=$(identify -format "%w" $i);
+		h=$(identify -format "%h" $i);
+		if [ "$w" -gt "$max_dimension" ] || [ "$h" -gt "$max_dimension" ]; 
+		then
+
+			convert $i -resize ${max_dimension}x${max_dimension} $i
+
+		fi
+		
+		case "$mime2" in
+			"png")
+				t=$(file $i | awk '{print $9}')
+				if [ "$t" == "RGB," ];
+				then
+					echo $?
+					# No tiene transparencia
+					convertirjpeg $i
+				fi
+				optipng -nc -nb -o7 -full -out optimizedImages/$i $i
+				;;
+			"jpeg")
+				jpegoptim --max=80 --all-progressive --strip-all --strip-exif --preserve --totals --dest=optimizedImages $i
+				;;
+		esac
+	fi
+done
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Script bash para optimización de imágenes en lotes. 
+
+## Descargo de responsabilidad
+
+Este script está desarrollado según unas necesidades personales particulares. 
+
+Si decides usarlo, será bajo tu propio riesgo. No me hago responsable de ninguna pérdida o deterioro de datos derivado del uso del script.
+
+En caso de que decidas usarlo, realiza siempre una copia de seguridad de las imágenes antes de procesarlas con el script.
